df7a41a2da81eb10765f52bcca0e0d6978ee43dc
Neandertalac <ivan.antolkovic@fer.hr>
2015-01-09 18:19:55 +0100

    Uređeni gumbi na početnoj stranici. Dodani listeneri za svaki gumb.

--- app/src/main/java/hr/fer/tel/ruazosa/iantolkovic/pametnakuca/MainActivity.java
+++ app/src/main/java/hr/fer/tel/ruazosa/iantolkovic/pametnakuca/MainActivity.java
@@ -1,7 +1,10 @@
 package hr.fer.tel.ruazosa.iantolkovic.pametnakuca;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -10,6 +13,43 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Typeface font = Typeface.createFromAsset( getAssets(), "fontawesome-webfont.ttf" );
+
+        Button temperatureBtn = (Button) findViewById(R.id.temperatureBtn);
+        Button airCondBtn = (Button) findViewById(R.id.airCondBtn);
+        Button lightBtn = (Button) findViewById(R.id.lightBtn);
+        Button settingsBtn = (Button) findViewById(R.id.settingsBtn);
+
+        settingsBtn.setTypeface(font);
+
+        temperatureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        airCondBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        lightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 }
